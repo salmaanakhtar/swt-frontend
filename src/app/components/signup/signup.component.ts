1f40b3d55b436ea605f3ec837ff2b5bc02a8f039
Salmaan Akhtar <akhtarsalmaan0@gmail.com>
2023-10-26 16:47:47 +0400

    Updated validation on signup page

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -17,7 +18,7 @@ export class SignupComponent {
   usernameError: string = '';
   emailError: string = '';
 
-  constructor(private apiService: ApiService, private router: Router, private snackBar: MatSnackBar) {}
+  constructor(private apiService: ApiService, private router: Router, private snackBar: MatSnackBar, private cdr: ChangeDetectorRef) {}
 
   onSignupSubmit(): void {
     const user = {
@@ -40,11 +41,9 @@ export class SignupComponent {
         this.router.navigate(['/']);
       }, error => {
         console.error('Signup failed:', error.error.error);
-        if (error.error.error === 'Username already exists') {
-          this.usernameError = 'Username already exists';
-        } else if (error.error.error === 'Email already exists') {
-          this.emailError = 'Email already exists';
-        }
+        this.usernameError = error.error.error.username;
+        this.emailError = error.error.error.email;
+        this.cdr.detectChanges();
       });
   }
 }
