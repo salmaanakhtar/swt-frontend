841aea0cf4263822b9a2f6e967506332e9e87e71
Salmaan Akhtar <akhtarsalmaan0@gmail.com>
2023-11-13 19:04:41 +0400

    Added a logout button, and changed color scheme

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,4 +13,14 @@ export class NavbarComponent {
   isActive(route: string): boolean {
     return this.router.isActive(route, true);
   }
+  logout(): void {
+    // Clear the local storage
+    localStorage.removeItem('jwtToken');
+    localStorage.removeItem('token');
+    localStorage.removeItem('userID');
+
+    // You can also navigate to a specific route after logout if needed
+    // For example, navigate to the home page '/'
+    this.router.navigate(['/']);
+  }
 }
